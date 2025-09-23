f58e5eb104942769575fa52ff2e3f159cb0d1f1c
Kevin-Galarza77 <jimenezkev1040@gmail.com>
2025-09-22 22:05:52 -0500

    Libraries are added to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
+import { providePrimeNG } from 'primeng/config';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideToastr } from 'ngx-toastr';
 import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import Aura from '@primeng/themes/aura';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false
+        }
+      }
+    }),
+    provideToastr()
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
